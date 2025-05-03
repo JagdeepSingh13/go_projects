35b29b5673e536cb1f04bfbce16b302e63dfc8fe
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-05-03 17:57:10 +0530

    basic server running and used FileServer

--- 01_server/main.go
+++ 01_server/main.go
@@ -1,10 +1,24 @@
 package main
 
+// on /, /form.html, /hello, /form
+
 import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "parseforn() error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST req. successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "name: %s\n", name)
+	fmt.Fprintf(w, "address: %s\n", address)
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
