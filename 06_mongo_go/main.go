7e613a81fa04f3ef507d77f1386980f2b9ce25e0
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-05-06 00:05:59 +0530

    ..

--- 06_mongo_go/main.go
+++ 06_mongo_go/main.go
@@ -1,34 +1,3 @@
-// package main
-
-// import (
-// 	"log"
-// 	"net/http"
-
-// 	"github.com/JagdeepSingh13/06_mongo_go/controllers"
-// 	"github.com/julienschmidt/httprouter"
-// 	"gopkg.in/mgo.v2"
-// )
-
-// func main() {
-// 	r := httprouter.New()
-// 	uc := controllers.NewuserController(getSession())
-
-// 	r.GET("/user/:id", uc.GetUser)
-// 	r.POST("/user", uc.CreateUser)
-// 	r.DELETE("/user/:id", uc.DeleteUser)
-
-// 	http.ListenAndServe("localhost:9000", r)
-// }
-
-// func getSession() *mgo.Session {
-// 	s, err := mgo.Dial("mongodb://127.0.0.1:27017?connect=direct")
-// 	if err != nil {
-// 		panic("Failed to connect to MongoDB: " + err.Error())
-// 	}
-// 	log.Println("Connected to MongoDB successfully")
-// 	return s
-// }
-
 package main
 
 import (
